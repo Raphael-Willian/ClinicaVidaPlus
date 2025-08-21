50c0ab631b7caf02e183daccd17c2c11f7c7f00a
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2025-08-20 22:07:12 -0300

    adicionado total de paciente(view,template)

--- pacientes/views.py
+++ pacientes/views.py
@@ -5,6 +5,7 @@ from .models import PacientesCadastrados
 def total_de_pacientes(request):
     if request.method == 'GET':
         lista_de_pacientes = PacientesCadastrados.objects.all() #lista todos os dados da tabela
-        return render(request, "pacientes.html", {"pacientes": lista_de_pacientes})
+        numero_de_pacientes = lista_de_pacientes.count()
+        return render(request, "pacientes.html", {"pacientes": lista_de_pacientes, "total": numero_de_pacientes})
 
 
