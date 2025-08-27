ced7046d0be1c9fbcc357310826b59a1121e2c71
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2025-08-27 13:01:32 -0300

    Adicionado urls consultas(settings.py)

--- clinica_vida_engine/urls.py
+++ clinica_vida_engine/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clinicavida/', include('pacientes.urls')),
+    path("clinicavida/", include("consultas.urls"))
 ]
